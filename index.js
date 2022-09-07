66fd064264eef4f464320e0a6a5a3778386c3618
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-09-07 17:29:22 +0300

    try/catch added

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ form.addEventListener("submit", function(event) {
     sendForm();
 });
 
-function sendForm() {
-    fetch("https://httpbin.org/post", {
+let div = document.createElement("div");
+div.style.color = "red";
+document.querySelector("#catForm").append(div);
+
+async function sendForm() {
+    div.innerHTML = ``;
+    try {
+        await fetch("https://httpbin.org/post", {
         method: 'POST',
         body: new FormData(form)
         })
-    .then(response => response.json())
-    .then(user => {
+        .then(response => response.json())
+        .then(user => {
         console.log(user);
-    })
-    .catch(error => console.log("При отправке данных произошла ошибка! Пожалуйста, повторите снова."));
+        })
+    }
+    catch (error) {
+        div.innerHTML = `<p>При отправке данных произошла ошибка! Пожалуйста, повторите снова.</p>`;
+        console.log("При отправке данных произошла ошибка! Пожалуйста, повторите снова.");
+        console.log(error.type);
+        console.log(error.message);
+    }
 }
